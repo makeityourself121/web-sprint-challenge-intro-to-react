155089b4eaec7bc18166bc9c38a35cb9a1f43e87
Dharmik Savaliya <danny@Dharmiks-MacBook-Pro.local>
2021-07-16 11:55:46 -0700

    done strcturing working on design

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,48 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Style=styled.div`
+width:80%;
+display: flex;
+justify-content: space-between;
+padding:8px;
+border:1px solid green;
+margin:2%auto;
+font-size: 25px;
+font-family: 'Geostar Fill', cursive;
+span{
+    border:1px solid black;
+    padding:5px;
+    border-radius: 10px 20px;
+    background-color: #696969;
+    color:#483D8B;
+    :hover{
+        background-color: #2F4F4F;
+        color:black;
+    }
+}
+:hover{
+    background-color: #D3D3D3;
+    color:#483D8B;
+}
+
+`
+export default function Character(props){
+    const {data}=props
+    
+    
+    return(
+        <div>
+            <style>
+@import url('https://fonts.googleapis.com/css2?family=Geostar+Fill&display=swap');
+</style>
+           <Style>{data.name} 
+           <span>{data.birth_year}</span>
+           </Style>
+        
+        </div>
+    )
+}
+
